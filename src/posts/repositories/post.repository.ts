051d8bd82ff456e93c3239befe6ab2af1e635522
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-05-07 22:24:39 -0300

    Relacionamentos prisma

--- src/posts/repositories/post.repository.ts
+++ src/posts/repositories/post.repository.ts
@@ -2,12 +2,40 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { CreatePostDto } from '../dto/create-post.dto';
 import { PostEntity } from '../entities/post.entity';
 import { UpdatePostDto } from '../dto/update-post.dto';
+import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { NotFoundError } from 'src/common/errors/types/NotFoundError';
 
+@Injectable()
 export class PostsRepository {
   constructor(private readonly prisma: PrismaService) {}
+
   async create(createPostDto: CreatePostDto): Promise<PostEntity> {
+    const { authorEmail } = createPostDto;
+
+    delete createPostDto.authorEmail;
+
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: authorEmail,
+      },
+    });
+
+    if (!user) {
+      throw new NotFoundError('Aurhot not found.');
+    }
+
+    const data: Prisma.PostCreateInput = {
+      ...createPostDto,
+      author: {
+        connect: {
+          email: authorEmail,
+        },
+      },
+    };
+
     return this.prisma.post.create({
-      data: createPostDto,
+      data,
     });
   }
 
